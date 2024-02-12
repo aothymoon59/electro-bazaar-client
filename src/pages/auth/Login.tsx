ef46cd41a0c7ab8b189793e1fcbe6cf1b58ef587
aothymoon59 <aothymoon59@gmail.com>
2024-02-13 02:44:12 +0600

    login done

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,22 +1,32 @@
-import { Button } from "antd";
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { FieldValues, useForm } from "react-hook-form";
 import { useLoginMutation } from "../../redux/features/auth/authApi";
 import { useAppDispatch } from "../../redux/hooks";
 import { setUser } from "../../redux/features/auth/authSlice";
 import { verifyToken } from "../../utils/verifyToken";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { useState } from "react";
+import { ImSpinner9 } from "react-icons/im";
 
 const Login = () => {
+  const [showPass, setShowPass] = useState(false);
   const navigate = useNavigate();
   const dispatch = useAppDispatch();
-  const { register, handleSubmit } = useForm({
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({
     defaultValues: {
       email: "messi@gmail.com",
       password: 123456,
     },
   });
 
-  const [login] = useLoginMutation();
+  const [login, { isLoading }] = useLoginMutation();
 
   const onSubmit = async (data: FieldValues) => {
     try {
@@ -28,23 +38,86 @@ const Login = () => {
       const res = await login(userInfo).unwrap();
       const user = verifyToken(res.data.accessToken);
       dispatch(setUser({ user, token: res.data.accessToken }));
+      reset();
+      toast.success(res.message);
       navigate("/");
-    } catch (error) {
-      console.log(error);
+    } catch (error: any) {
+      toast.error(error.data.message);
     }
   };
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div>
-        <label htmlFor="email">Email:</label>
-        <input type="email" id="email" {...register("email")} />
+    <div className="p-3 h-screen flex justify-center items-center">
+      <div className="bg-purple-100 p-5 rounded-md shadow-md w-full max-w-[400px]">
+        <div className="text-center mb-5">
+          <h2 className="text-xl font-normal mb-2">
+            Welcome To{" "}
+            <span className="font-bold text-primary-main">ElectroMart</span>
+          </h2>
+          <p className="text-sm">Please enter your credentials to login</p>
+        </div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="form-control mb-4">
+            <label className="label">
+              <span className="label-text font-medium">Email*</span>
+            </label>
+            <input
+              type="email"
+              {...register("email", { required: true })}
+              name="email"
+              placeholder="example@gmail.com"
+              className="input input-bordered w-full"
+            />
+            {errors.email && (
+              <span className="text-red-600 mt-2">Email is required</span>
+            )}
+          </div>
+          <div className="form-control mb-6 relative">
+            <label className="label">
+              <span className="label-text font-medium">Password*</span>
+            </label>
+            <input
+              type={showPass ? "text" : "password"}
+              {...register("password", {
+                required: true,
+              })}
+              name="password"
+              placeholder="******"
+              className="input input-bordered w-full"
+            />
+            {errors.password?.type === "required" && (
+              <p className="text-red-600 mt-2">Password is required</p>
+            )}
+            <p
+              className="absolute top-[48px] right-[15px]"
+              onClick={() => setShowPass(!showPass)}
+            >
+              <small>{showPass ? <FaEye /> : <FaEyeSlash />}</small>
+            </p>
+          </div>
+          <button
+            className={`primary-main-btn w-full hover:bg-transparent hover:text-[#8850B3] transition-all duration-200 ease-in-out ${
+              isLoading && "cursor-not-allowed"
+            }`}
+            disabled={isLoading}
+            type="submit"
+          >
+            {isLoading ? (
+              <ImSpinner9 className="m-auto animate-spin" />
+            ) : (
+              "Login"
+            )}
+          </button>
+          <p className="text-center mt-4 mb-6">
+            <small>
+              New user?{" "}
+              <Link to="/register" className="font-semibold text-primary-main">
+                Register
+              </Link>
+            </small>
+          </p>
+        </form>
       </div>
-      <div>
-        <label htmlFor="password">Password:</label>
-        <input type="text" id="password" {...register("password")} />
-      </div>
-      <Button htmlType="submit">Login</Button>
-    </form>
+    </div>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
@@ -16,7 +17,7 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
   corePlugins: {
     preflight: false,
   },
