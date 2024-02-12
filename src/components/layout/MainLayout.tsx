cf40332f7782cbcdfa082d043932e91d3c3623a2
aothymoon59 <aothymoon59@gmail.com>
2024-02-13 03:22:53 +0600

    logout functionality added

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -2,10 +2,13 @@ import { Layout } from "antd";
 import { Outlet } from "react-router-dom";
 import { IoLogOut } from "react-icons/io5";
 import Sidebar from "./Sidebar";
+import { useAppDispatch } from "../../redux/hooks";
+import { setUser } from "../../redux/features/auth/authSlice";
 
 const { Header, Content } = Layout;
 
 const MainLayout = () => {
+  const dispatch = useAppDispatch();
   return (
     <Layout style={{ height: "100vh" }}>
       <Sidebar />
@@ -14,7 +17,10 @@ const MainLayout = () => {
           className="bg-primary-main flex justify-end items-center"
           style={{ padding: "0 20px" }}
         >
-          <button className="flex items-center gap-1 cursor-pointer bg-transparent border-0 text-white text-sm lg:text-lg">
+          <button
+            onClick={() => dispatch(setUser({ user: null, token: null }))}
+            className="flex items-center gap-1 cursor-pointer bg-transparent border-0 text-white text-sm lg:text-lg"
+          >
             <span>Logout</span>
             <IoLogOut />
           </button>
