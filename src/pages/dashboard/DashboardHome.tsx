93dbb230afe10c342005ea8dfc847aef2b343c7f
aothymoon59 <aothymoon59@gmail.com>
2024-02-17 03:50:56 +0600

    done

--- src/pages/dashboard/DashboardHome.tsx
+++ src/pages/dashboard/DashboardHome.tsx
@@ -1,5 +1,12 @@
 const DashboardHome = () => {
-  return <div>Dashboard home</div>;
+  return (
+    <div>
+      <h2 className="text-center font-medium text-2xl md:text-4xl">
+        Welcome to{" "}
+        <span className="font-bold text-primary-main">ElectroMart</span>
+      </h2>
+    </div>
+  );
 };
 
 export default DashboardHome;
