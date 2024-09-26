c1b0939ef85f91f98ec0c5dcf51bb5e487d9b9ec
aothymoon59 <aothymoon59@gmail.com>
2024-09-26 11:12:51 +0600

    [UPDATE] color theme in auth pages

--- src/pages/auth/Register.tsx
+++ src/pages/auth/Register.tsx
@@ -45,8 +45,8 @@ const Register = () => {
   };
 
   return (
-    <div className="lg:p-3 min-h-screen flex justify-center items-center bg-purple-100 lg:bg-transparent">
-      <div className="bg-purple-100 p-5 rounded-md sm:shadow-md w-full max-w-[400px]">
+    <div className="lg:p-3 min-h-screen flex justify-center items-center bg-primary-light lg:bg-transparent">
+      <div className="bg-primary-lighter p-5 rounded-md sm:shadow-md w-full max-w-[400px]">
         <div className="text-center mb-5">
           <h2 className="text-xl font-normal mb-2">
             Welcome To{" "}
