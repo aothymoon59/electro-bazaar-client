aa751b482ec6b0e056a4eb94bc2a5c42a0afc72b
aothymoon59 <aothymoon59@gmail.com>
2024-09-27 12:16:13 +0600

    [ADD] vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,7 @@ import router from "./routes/routes.tsx";
 import { PersistGate } from "redux-persist/integration/react";
 import { Toaster } from "react-hot-toast";
 import { ConfigProvider } from "antd";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
@@ -32,6 +33,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
         <PersistGate loading={null} persistor={persistor}>
           <RouterProvider router={router} />
           <Toaster />
+          <Analytics />
         </PersistGate>
       </ConfigProvider>
     </Provider>
