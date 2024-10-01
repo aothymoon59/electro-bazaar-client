0b04b7a976b9e0f9b69142a581839e20667de996
aothymoon59 <aothymoon59@gmail.com>
2024-10-01 13:16:12 +0600

    [REFACTOR] donut chart

--- src/components/dashboardHome/charts/SoldPieChart.tsx
+++ src/components/dashboardHome/charts/SoldPieChart.tsx
@@ -2,21 +2,37 @@
 import ReactApexChart from "react-apexcharts";
 
 const SoldPieChart = () => {
-  const series = [44, 55, 41, 17, 15];
+  const data = [74, 55, 41, 17, 15];
 
   const options: any = {
     chart: {
       type: "donut",
     },
+    plotOptions: {
+      pie: {
+        donut: {
+          labels: {
+            show: true,
+          },
+          size: "50%",
+        },
+        expandOnClick: true,
+      },
+    },
+    dataLabels: {
+      enabled: true,
+    },
+    legend: {
+      show: true,
+      position: "bottom",
+    },
+    labels: ["Smartphones", "Laptops", "Tablets", "Wearables", "Audio Devices"],
     responsive: [
       {
         breakpoint: 480,
         options: {
           chart: {
             width: 200,
-          },
-          legend: {
-            position: "bottom",
           },
         },
       },
@@ -26,12 +42,12 @@ const SoldPieChart = () => {
   return (
     <div className="h-[450px] xl:h-[550px] mt-6 bg-primary-lighter p-5 flex flex-col items-start gap-5">
       <div>
-        <h3 className="text-lg font-semibold">Most Sold Items </h3>
+        <h3 className="text-lg font-semibold">Most Sold Categories</h3>
       </div>
       <div className="w-full h-full flex justify-center items-center">
         <ReactApexChart
           options={options}
-          series={series}
+          series={data}
           type="donut"
           className="w-full"
           height={300}
