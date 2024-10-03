b36f1ffc8c1c7e23270132e7ae9a380462e1d976
aothymoon59 <aothymoon59@gmail.com>
2024-10-03 23:52:16 +0600

    [USE] FromTop to main wrapper component

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -7,6 +7,7 @@ import { currentUser } from "../../redux/features/auth/authSlice";
 import { useEffect, useState } from "react";
 import moment from "moment";
 import { FaUserCircle } from "react-icons/fa";
+import FromTop from "../global/loaders/FromTop";
 
 const { Header, Content } = Layout;
 
@@ -24,28 +25,32 @@ const MainLayout = () => {
   }, []);
 
   return (
-    <Layout className="h-screen">
-      <Sidebar />
-      <Layout>
-        <Header
-          className="bg-primary-main flex justify-end sm:justify-between items-center gap-2 sticky top-0 z-10"
-          style={{ padding: "0 20px" }}
-        >
-          <div className="hidden sm:block">
-            <p className="text-[13px] text-white font-medium">{currentTime}</p>
-          </div>
-          <div className="flex items-center gap-1 font-medium text-white">
-            <FaUserCircle className="text-3xl" />
-            <span className="text-[15px]">{user?.name?.split(" ")?.[0]}</span>
-          </div>
-        </Header>
-        <Content className="rounded-lg p-[24px_16px_0px] bg-primary-lighter max-h-screen overflow-y-auto">
-          <div className="p-4 min-h-[85vh] bg-primary-light rounded-lg">
-            <Outlet />
-          </div>
-        </Content>
+    <FromTop>
+      <Layout className="h-screen">
+        <Sidebar />
+        <Layout>
+          <Header
+            className="bg-primary-main flex justify-end sm:justify-between items-center gap-2 sticky top-0 z-10"
+            style={{ padding: "0 20px" }}
+          >
+            <div className="hidden sm:block">
+              <p className="text-[13px] text-white font-medium">
+                {currentTime}
+              </p>
+            </div>
+            <div className="flex items-center gap-1 font-medium text-white">
+              <FaUserCircle className="text-3xl" />
+              <span className="text-[15px]">{user?.name?.split(" ")?.[0]}</span>
+            </div>
+          </Header>
+          <Content className="rounded-lg p-[24px_16px_0px] bg-primary-lighter max-h-screen overflow-y-auto">
+            <div className="p-4 min-h-[85vh] bg-primary-light rounded-lg">
+              <Outlet />
+            </div>
+          </Content>
+        </Layout>
       </Layout>
-    </Layout>
+    </FromTop>
   );
 };
 
