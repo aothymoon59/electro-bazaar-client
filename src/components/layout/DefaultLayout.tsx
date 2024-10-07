5bbd8de4c590df49231fcd62138feec6ff364620
aothymoon59 <aothymoon59@gmail.com>
2024-10-07 10:44:25 +0600

    [REFACTOR] Mianlayout, HEader and Sidebar

--- src/components/layout/DefaultLayout.tsx
+++ src/components/layout/DefaultLayout.tsx
@@ -7,30 +7,26 @@ const DefaultLayout = () => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
   return (
-    <div className="">
-      {/* <!-- ===== Page Wrapper Start ===== --> */}
-      <div className="flex h-screen overflow-hidden">
-        {/* <!-- ===== Sidebar Start ===== --> */}
-        <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-        {/* <!-- ===== Sidebar End ===== --> */}
+    <div className="flex h-screen overflow-hidden">
+      {/* <!-- ===== Sidebar Start ===== --> */}
+      <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+      {/* <!-- ===== Sidebar End ===== --> */}
 
-        {/* <!-- ===== Content Area Start ===== --> */}
-        <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
-          {/* <!-- ===== Header Start ===== --> */}
-          <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-          {/* <!-- ===== Header End ===== --> */}
+      {/* <!-- ===== Content Area Start ===== --> */}
+      <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
+        {/* <!-- ===== Header Start ===== --> */}
+        <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+        {/* <!-- ===== Header End ===== --> */}
 
-          {/* <!-- ===== Main Content Start ===== --> */}
-          <main className="rounded-lg p-[24px_16px_0px] bg-primary-lighter">
-            <div className="p-4 min-h-[85vh] bg-primary-light rounded-lg">
-              <Outlet />
-            </div>
-          </main>
-          {/* <!-- ===== Main Content End ===== --> */}
-        </div>
-        {/* <!-- ===== Content Area End ===== --> */}
+        {/* <!-- ===== Main Content Start ===== --> */}
+        <main className="rounded-lg p-[24px_16px_0px] bg-primary-lighter">
+          <div className="p-4 min-h-[85vh] bg-primary-light rounded-lg">
+            <Outlet />
+          </div>
+        </main>
+        {/* <!-- ===== Main Content End ===== --> */}
       </div>
-      {/* <!-- ===== Page Wrapper End ===== --> */}
+      {/* <!-- ===== Content Area End ===== --> */}
     </div>
   );
 };
--- src/components/shared/Sidebar/index.tsx
+++ src/components/shared/Sidebar/index.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import logo from "../../../assets/icons/lamp.png";
 import { FaArrowLeft } from "react-icons/fa6";
@@ -12,22 +11,8 @@ interface SidebarProps {
 }
 
 const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
-  const trigger = useRef(null);
-  const sidebar = useRef(null);
-
-  // close if the esc key is pressed
-  useEffect(() => {
-    const keyHandler = ({ keyCode }: KeyboardEvent) => {
-      if (!sidebarOpen || keyCode !== 27) return;
-      setSidebarOpen(false);
-    };
-    document.addEventListener("keydown", keyHandler);
-    return () => document.removeEventListener("keydown", keyHandler);
-  });
-
   return (
     <aside
-      ref={sidebar}
       className={`absolute left-0 top-0 z-[9999] flex h-screen w-72.5 flex-col overflow-y-hidden bg-primary-main duration-300 ease-linear lg:static lg:translate-x-0 ${
         sidebarOpen ? "translate-x-0" : "-translate-x-full"
       }`}
@@ -37,15 +22,12 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
         <Link to="/">
           <div className="flex justify-center items-center gap-1 text-white">
             <img className="w-7 h-7" src={logo} alt="logo" />
-            <h1>ElectroBazaar</h1>
+            <h3>ElectroBazaar</h3>
           </div>
         </Link>
         <button
-          ref={trigger}
           onClick={() => setSidebarOpen(!sidebarOpen)}
           className="block lg:hidden bg-primary-main px-2 py-1"
-          aria-controls="sidebar"
-          aria-expanded={sidebarOpen}
         >
           <FaArrowLeft className="text-white" />
         </button>
