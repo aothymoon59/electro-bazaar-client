cd536f78019b3d07c1a87c32beaf33041a550dfd
aothymoon59 <aothymoon59@gmail.com>
2024-10-08 12:34:23 +0600

    [ADD] UnAuthorized Component page

---
+++ src/pages/errorPage/UnAuthorized.tsx
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom";
+import { Result } from "antd";
+//Error git page
+const ErrorPage = () => {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <Result
+        status="403"
+        title="403"
+        subTitle="Sorry, you are not authorized to access this page.."
+        extra={
+          <Link
+            to="/"
+            className="primary-main-btn no-underline hover:bg-opacity-80 transition-all duration-200 ease-in-out"
+          >
+            Back to home
+          </Link>
+        }
+      />
+    </div>
+  );
+};
+
+export default ErrorPage;
